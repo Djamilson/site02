a633d27c1ba3e1b4aa1e6c2e051d5f60ed014b23
Djamilson <djamilson@gmail.com>
2020-02-13 01:12:35 -0300

    Teste Gatsby 03

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import useMenu from '../useMenu';
+
+import * as S from './styled';
+
+const Navigation = ({ isActive, handleToggleMenu }) => {
+  const menuItems = useMenu();
+  
+  return (
+    <>
+      <S.Navigation className={isActive ? 'active' : ''}>
+        {menuItems.map(menu => (
+          <S.NavigationLink
+            to={menu.link}
+            aria-label={menu.name}
+            activeClassName="active"
+            onClick={() => handleToggleMenu()}>
+            {menu.name}
+          </S.NavigationLink>
+        ))}
+
+      </S.Navigation>
+    </>
+  );
+};
+
+export default Navigation;
