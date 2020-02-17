bde5ca107ef40dce445a1242b54250d052b5e000
Djamilson <djamilson@gmail.com>
2020-02-17 00:56:12 -0300

    Mudando as imagens

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../images/logo-iceberg.svg';
+import logo from '../../images/logo-iceberg.jpeg';
 
 const Logo = () => {
   return <img src={logo} alt="Iceberg Logo" />;
